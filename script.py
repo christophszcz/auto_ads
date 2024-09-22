c14e5cd9cc2ed5bba1c030e402769aebb5acb3b4
Chris Szczechowicz <info@cjszczechowicz.com>
2024-09-21 23:18:53 -0400

    Create a login function and refactor code

--- script.py
+++ script.py
@@ -10,26 +10,29 @@ DRIVER_PATH =  os.environ['DRIVER_PATH']
 EMAIL = os.environ['EMAIL']
 PASSWORD = os.environ['PASSWORD']
 
-driver = webdriver.Chrome(DRIVER_PATH)
+def login():
+    driver = webdriver.Chrome(DRIVER_PATH)
 
-driver.get(LOGIN_URL)
+    driver.get(LOGIN_URL)
 
-time.sleep(5)
+    time.sleep(5)
 
-email_field = driver.find_element(By.ID, 'LoginEmailOrNickname')
-password_field = driver.find_element(By.ID, 'login-password')
-login_button = driver.find_element(By.XPATH, '//button[@type="submit" and text()="Log In"]')
+    email_field = driver.find_element(By.ID, 'LoginEmailOrNickname')
+    password_field = driver.find_element(By.ID, 'login-password')
+    login_button = driver.find_element(By.XPATH, '//button[@type="submit" and text()="Log In"]')
 
-email_field.send_keys(EMAIL)
-password_field.send_keys(PASSWORD)
+    email_field.send_keys(EMAIL)
+    password_field.send_keys(PASSWORD)
 
-login_button.click()
-time.sleep(5)
+    login_button.click()
+    time.sleep(5)
 
-try:
-    user_icon = driver.find_element(By.XPATH, '//a[@title="My Kijiji"]')
-    print("Login successful")
-except Exception as e:
-    print("Login failed", e)
+    try:
+        user_icon = driver.find_element(By.XPATH, '//a[@title="My Kijiji"]')
+        print("Login successful")
+    except Exception as e:
+        print("Login failed", e)
 
-driver.quit()
+    driver.quit()
+
+login()
